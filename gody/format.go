92edea6e428f78161daf3368906f5eeeee6159dc
wataru-kurashima <wataru-kurashima@hands-lab.com>
2017-11-22 19:06:34 +0900

    format.go xsv json

--- gody/format.go
+++ gody/format.go
@@ -3,38 +3,45 @@ package gody
 import (
 	"fmt"
 	"strings"
+	"encoding/json"
 )
 
 var (
-	head []string
-	body []string
+	head      []string
+	body      []string
+	delimiter string
 )
 
 func Format(ddbresult map[string]interface{}, format string, header bool) {
 	switch format {
 	case "ssv":
-		ssv(ddbresult, header)
-		//case "csv":
-		//	csv(ddbresult, header)
-		//case "tsv":
-		//	tsv(ddbresult, header)
-		//case "json":
-		//	json(ddbresult, header)
+		xsv(ddbresult, header, " ")
+	case "csv":
+		xsv(ddbresult, header, ",")
+	case "tsv":
+		xsv(ddbresult, header, "\t")
+	case "json":
+		toJson(ddbresult)
 	}
 }
 
-func ssv(ddbresult map[string]interface{}, header bool) {
+func xsv(ddbresult map[string]interface{}, header bool, delimiter string) {
 	if header {
 		for k, v := range ddbresult {
 			head = append(head, k)
 			body = append(body, fmt.Sprint(v))
 		}
-		fmt.Println(strings.Join(head, " "))
-		fmt.Println(strings.Join(body, " "))
+		fmt.Println(strings.Join(head, delimiter))
+		fmt.Println(strings.Join(body, delimiter))
 	} else {
 		for _, v := range ddbresult {
 			body = append(body, fmt.Sprint(v))
 		}
-		fmt.Println(strings.Join(body, " "))
+		fmt.Println(strings.Join(body, delimiter))
 	}
+}
+
+func toJson(ddbresult map[string]interface{}) {
+	jsonString, _ := json.Marshal(ddbresult)
+	fmt.Println(string(jsonString))
 }
