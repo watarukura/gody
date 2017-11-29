d69198d5f852b0455a1605438d53cf6c24332f47
wataru-kurashima <wataru-kurashima@hands-lab.com>
2017-11-29 13:34:56 +0900

    get_test.go

---
+++ cmd/get_test.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"testing"
+	"bytes"
+	"strings"
+	"fmt"
+)
+
+func TestGet(t *testing.T) {
+	cases := []struct {
+		command string
+		want    string
+	}{
+		//{command: "gody get", want: "Get one record"},
+		{command: "gody get --pkey aaa", want: "Error: Parameter error: TableName is required"},
+	}
+
+	for _,c := range cases {
+		buf := new(bytes.Buffer)
+		cmd := NewCmdRoot()
+		cmd.SetOutput(buf)
+		cmdArgs := strings.Split(c.command, " ")
+		fmt.Printf("cmdArgs %+v\n", cmdArgs)
+		cmd.SetArgs(cmdArgs[1:])
+		cmd.Execute()
+
+		get := buf.String()
+		lineOne := strings.Split(get, "\n")[0]
+		if c.want != lineOne {
+			t.Errorf("unexpected response: want:%+v, get:%+v", c.want, get)
+		}
+	}
+}
